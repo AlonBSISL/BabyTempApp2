9ad93b9f1997a19e6617b5715c9b7b08bfda4f6b
AlonBSISL <abarshalom@islstudent.ch>
2023-01-09 14:56:56 +0100

    commit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ baby_list_window = None
 counter = 0
 
 
-#This function saves the new baby into the system and
+# This function saves the new baby into the system and makes sure warnings are issued if necessary.
 def save_baby():
     global add_new_baby, app, baby, babies, time, current_time, timerstarted, counter, id
     baby = [add_new_baby.name_box.value, id, add_new_baby.current_temp_box.value]
@@ -38,6 +38,7 @@ def save_baby():
     app.focus()
 
 
+# This allows the user to add a new baby and its basic information and gives the user the baby's ID.
 def open_add_new_baby():
     global name_box, parent_name_box, current_temp_box, add_new_baby, save_button, id_show
     add_new_baby = Window(app, title="Add new baby", bg="#f7ff00")
@@ -53,7 +54,7 @@ def open_add_new_baby():
     id_show = Text(add_new_baby, font="Noto Serif Telugu", text="Baby's ID is: " + str(id), size=14,
                    color="#7700C0")
     save_button = PushButton(add_new_baby, command=save_baby, text="Save and close")
-   # add_new_baby.set_full_screen()
+    # add_new_baby.set_full_screen()
 
     add_new_baby.name_box.clear()
     add_new_baby.current_temp_box.clear()
@@ -68,11 +69,12 @@ def clear_list_window():
     app.focus()
 
 
+# This is a list of babies and their information which allows the user to scroll through the babies and, when clicking on one, add new measurements and see statistics.
 def open_baby_list():
     global baby_list_window
     baby_list_window = Window(app, title="Baby list", bg="#f7ff00")
 
-    #baby_list_window.set_full_screen()
+    # baby_list_window.set_full_screen()
     main_title_list = Text(baby_list_window, text="Baby temperature app", align="top", color="#7700C0",
                            font="Green London", size=50)
     pushdown_4 = Box(baby_list_window, align="top", height=50)
@@ -81,8 +83,7 @@ def open_baby_list():
     exit_button = PushButton(baby_list_window, command=clear_list_window, text="Close")
     baby_list_window.show(wait=True)
     baby_list_window.focus()
-
-
+#This saves a new temperature added through clicking one of the babies in the list. In addition, warnings are given if necessary.
 def save_temp():
     global current_baby, add_temp, babies, baby_list_window, baby_list, counter
     idx = babies.index(current_baby)
@@ -124,7 +125,7 @@ def add_temp_check(x):
     current_id = current_baby[1]
     add_temp_check_window = Window(app, title="ID", bg="#f7ff00")
     add_temp_check_window.show(wait=True)
-    #add_temp_check_window.set_full_screen()
+    # add_temp_check_window.set_full_screen()
     main_title_list = Text(add_temp_check_window, text="Baby temperature app", align="top", color="#7700C0",
                            font="Green London", size=50)
     pushdown_5 = Box(add_temp_check_window, align="top", height=50)
@@ -139,7 +140,7 @@ def add_temp_check(x):
 
 
 app = App(layout="auto", title="Baby Temperature app", bg="#f7ff00")
-#app.set_full_screen()
+# app.set_full_screen()
 main_title = Text(app, text="Baby temperature app", align="top", color="#7700C0", font="Green London", size=100)
 
 pushdown_1 = Box(app, align="top", height=220)
@@ -166,6 +167,5 @@ quit_button.font = "Noto Serif Telugu"
 if timerstarted is False:
     timer.start()
     timerstarted = True
-
 
 app.display()
