9401034c3eb4af2513d514cfe51377ac3dadcc59
AlonBSISL <abarshalom@islstudent.ch>
2023-01-09 15:37:59 +0100

    commit

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def open_add_new_baby():
     id_show = Text(add_new_baby, font="Noto Serif Telugu", text="Baby's ID is: " + str(id), size=14,
                    color="#7700C0")
     save_button = PushButton(add_new_baby, command=save_baby, text="Save and close")
-    # add_new_baby.set_full_screen()
+    add_new_baby.set_full_screen()
 
     add_new_baby.name_box.clear()
     add_new_baby.current_temp_box.clear()
@@ -76,7 +76,7 @@ def open_baby_list():
     global baby_list_window
     baby_list_window = Window(app, title="Baby list", bg="#f7ff00")
 
-    # baby_list_window.set_full_screen()
+    baby_list_window.set_full_screen()
     main_title_list = Text(baby_list_window, text="Baby temperature app", align="top", color="#7700C0",
                            font="Green London", size=50)
     pushdown_4 = Box(baby_list_window, align="top", height=50)
@@ -89,7 +89,7 @@ def open_baby_list():
 
 # This saves a new temperature added through clicking one of the babies in the list. In addition, warnings are given if necessary.
 def save_temp():
-    global current_baby, add_temp, babies, baby_list_window, baby_list, counter
+    global current_baby, add_temp, babies, baby_list_window, baby_list, counter, c
     idx = babies.index(current_baby)
     current_baby.append(add_temp.value)
     babies[idx] = current_baby
@@ -122,7 +122,7 @@ def save_temp():
 
 # This window allows the user to add a new temperature for their baby.
 def add_temp_check(x):
-    global current_temp_box_add, add_temp_check_window, baby_id_check, list_id, current_baby, add_temp
+    global current_temp_box_add, add_temp_check_window, baby_id_check, list_id, current_baby, add_temp, c
     print(x)
     print(type(x))
     list_id = x[0]
@@ -130,7 +130,7 @@ def add_temp_check(x):
     current_id = current_baby[1]
     add_temp_check_window = Window(app, title="ID", bg="#f7ff00")
     add_temp_check_window.show(wait=True)
-    # add_temp_check_window.set_full_screen()
+    add_temp_check_window.set_full_screen()
     main_title_list = Text(add_temp_check_window, text="Baby temperature app", align="top", color="#7700C0",
                            font="Green London", size=50)
     pushdown_5 = Box(add_temp_check_window, align="top", height=50)
@@ -138,6 +138,7 @@ def add_temp_check(x):
                         text="Baby's highest temperature recorded: " + max(current_baby[2:]))
     lowest_temp = Text(add_temp_check_window, font="Noto Serif Telugu",
                        text="Baby's lowest temperature recorded: " + min(current_baby[2:]))
+
     add_temp_text = Text(add_temp_check_window, text="Add new temperature:", font="Noto Serif Telugu", size=14,
                          color="#7700C0")
     add_temp = TextBox(add_temp_check_window, height=20, width=30)
@@ -146,7 +147,7 @@ def add_temp_check(x):
 
 # This is the app's menu
 app = App(layout="auto", title="Baby Temperature app", bg="#f7ff00")
-# app.set_full_screen()
+app.set_full_screen()
 main_title = Text(app, text="Baby temperature app", align="top", color="#7700C0", font="Green London", size=100)
 
 pushdown_1 = Box(app, align="top", height=220)
